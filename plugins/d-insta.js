b76521381af0db6f5e60599f57659c4fb1f6c987
MAHI-YT <scarystories576@gmail.com>
2025-12-23 12:31:41 +0500

    Update d-insta.js

--- plugins/d-insta.js
+++ plugins/d-insta.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, args, q, reply, react }) => {
         // Send video to chat
         await conn.sendMessage(from, {
             video: Buffer.from(response.data),
-            caption: `🎥 > *IT'S ERFAN AHMAD*`,
+            caption: ` > *IT'S ERFAN AHMAD*`,
             mimetype: 'video/mp4'
         }, { quoted: mek });
 
